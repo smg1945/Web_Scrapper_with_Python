87a00da8b5f17f454a5034c67abb7ee6e5d9d1ae
smg1945 <saojung518@naver.com>
2020-09-27 23:19:25 +0900

    Scrapper_with_Web

--- save.py
+++ save.py
@@ -3,7 +3,7 @@ import csv
 def save_to_file(jobs):
     file = open("jobs.csv", mode="w", encoding="utf-8")
     writer = csv.writer(file)
-    writer.writerow(["title", "company", "location", "link"])
+    writer.writerow(["Title", "Company", "Location", "Link"])
     for job in jobs:
         writer.writerow(list(job.values()))
     return
--- so.py
+++ so.py
@@ -1,12 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-LIMIT = 50
-URL = f"https://stackoverflow.com/jobs?q=python&sort=i"
 
-
-def get_last_page():
-    result = requests.get(URL)
+def get_last_page(url):
+    result = requests.get(url)
     soup = BeautifulSoup(result.text, "html.parser")
     pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
     last_page = pages[-2].get_text(strip=True)
@@ -15,8 +12,7 @@ def get_last_page():
 
 def extract_job(html):
     title = html.find("h2").find("a")["title"]
-    company, location = html.find(
-        "h3", {"class": "fc-black-700"}).find_all("span", recursive=False)
+    company, location = html.find("h3").find_all("span", recursive=False)
     company = company.get_text(strip=True)
     location = location.get_text(strip=True)
     job_id = html['data-jobid']
@@ -24,11 +20,11 @@ def extract_job(html):
             "apply_link": f"https://stackoverflow.com/jobs/{job_id}"}
 
 
-def extract_jobs(last_page):
+def extract_jobs(last_page, url):
     jobs = []
     for page in range(last_page):
         print(f"Scrapping SO: Page: {page}")
-        result = requests.get(f"{URL}&pg={page+1}")
+        result = requests.get(f"{url}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
         for result in results:
@@ -37,7 +33,8 @@ def extract_jobs(last_page):
     return jobs
 
 
-def get_jobs():
-    last_page = get_last_page()
-    jobs = extract_jobs(last_page)
+def get_jobs(word):
+    url = f"https://stackoverflow.com/jobs?q={word}&sort=i"
+    last_page = get_last_page(url)
+    jobs = extract_jobs(last_page, url)
     return jobs
